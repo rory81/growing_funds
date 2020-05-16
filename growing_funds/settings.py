169d784e6b3692d5ff02ccc14d29890c0432ca7c
rory81 <nbleijie@gmail.com>
2020-05-16 13:28:00 +0200

    removed bug with using absolute for calculating days and fixed some layout issues

--- growing_funds/settings.py
+++ growing_funds/settings.py
@@ -132,3 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
