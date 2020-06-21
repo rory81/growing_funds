c875b16431d8a172433e23da3d26b464313bfa42
rory81 <nbleijie@gmail.com>
2020-06-21 18:50:43 +0200

    fix the project urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
+from django.urls import path
 from .views import get_projects, project_detail, create_or_edit_project, get_project_category
 
 
 urlpatterns = [
-    url(r'^$', get_projects, name = 'get_projects'),
-    url(r'^(?P<pk>\d+)/$', project_detail, name = 'project_detail'),
-    url(r'^new/$', create_or_edit_project, name = 'start_project'),
-    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_project, name = 'edit_project'),
-    url(r'^category=(?P<project_category>\w+)$', get_project_category, name = 'per_category'),
+    path('', get_projects, name = 'get_projects'),
+    path('<int:pk>', project_detail, name = 'project_detail'),
+    path('new/', create_or_edit_project, name = 'start_project'),
+    path('<int:pk>/edit/', create_or_edit_project, name = 'edit_project'),
+    path('category=<str:project_category>', get_project_category, name = 'per_category'),
     
 ]
