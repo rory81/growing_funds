e4f8e219568b0dca5b2783bc5546dc9d8499b7b8
rory81 <nbleijie@gmail.com>
2020-07-21 22:53:03 +0200

    redid stripe to video5

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import create_session
+from .views import checkout
 
 
 
 urlpatterns = [
     
-    path('<int:pk>', create_session, name = 'checkout'),    
+    path('<int:pk>', checkout, name ='checkout'),    
     
 ]
