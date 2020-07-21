f1aeddf090dfe7d21a620e737af0cdf95c40bb54
rory81 <nbleijie@gmail.com>
2020-07-21 19:45:54 +0200

    change stripe method - setup

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import checkout
+from .views import create_session
+
 
 
 urlpatterns = [
     
-    path('<int:pk>', checkout, name = 'checkout'),
-    
+    path('<int:pk>', create_session, name = 'checkout'),    
     
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,44 +1,45 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
+import json
 from .models import Order
 from .forms import OrderForm
 from projects.models import Project
 
 import stripe
 
-def checkout(request, pk=None):
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_SECRET_KEY
+def success(request):
+    return render(request, 'payment_success.html')
 
+def cancel(request):
+    return render(request, 'payment_success.html')
+
+def create_session(request, pk=None):
     project = get_object_or_404(Project, pk=pk) if pk else None
     if request.method == 'POST':
-        order_form = OrderForm(request.POST)
-        total = int(order_form['amount_pledged'].value())
-        
-
-    else: 
-        order_form = OrderForm()
-        total=1
-
-    stripe_total = round(total * 100)
-    stripe.api_key =stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
+        data = json.loads(request.data)
+        session = stripe.checkout.Session.create(
+            success_url='http://localhost:127.0.0.1/success?id={CHECKOUT_SESSION_ID}',
+            cancel_url='http://localhost:127.0.0.1/cancel',
+            submit_type='donate',
+            payment_method_types=['card'],
+            line_items=[{
+            'amount': data['amount'],
+            'name': 'Pledge',
+            'billing_address_collection': required,
+            'currency': 'USD',
+            'quantity': 1
+            }],
+            metadata={
+            'cause': data['cause'],
+            }
         )
-    
+        return jsonify(session)
+    return render(request, 'checkout.html', {'project':project})
 
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-        Did you forget to set it in your environment?')
-
-    context = {
-        'order_form': order_form,
-        'project':project,
-        'stripe_public_key':stripe_public_key,
-        'client_secret':intent.client_secret
-    }
-
-    return render(request, 'checkout.html', context)
-
+def get_session(request):
+    session = stripe.checkout.Session.retrieve(
+        request.args['id'],
+        expand=['payment-intent'],
+    )
+    return jsonify(session)
