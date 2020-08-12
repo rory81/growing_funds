934cd421e204189f82607fdf005d4362307af5da
Nathalie <nbleijie@gmail.com>
2020-08-12 16:19:18 +0000

    changes after css and html validator

--- checkout/views.py
+++ checkout/views.py
@@ -16,11 +16,16 @@ stripe.api_key = stripe_secret_key
 
 
 def checkout(request, pk=None):
+    """
+    view to get the checkoutform and prefill the form if possible
+    """
     project = get_object_or_404(Project, pk=pk) if pk else None
 
     if request.user.is_authenticated:
         try:
+            # get the user that is signed in
             profile = UserProfile.objects.get(user=request.user)
+            # prefill the form with the user info from that specific user
             order_form = OrderForm(initial={
                 'full_name': profile.user.get_full_name(),
                 'email': profile.user.email,
@@ -33,8 +38,10 @@ def checkout(request, pk=None):
                 'county': profile.default_county,
 
             })
+        # if this user hasn't filled their user info yet, give them an empty form to fill in
         except UserProfile.DoesNotExist:
             order_form = OrderForm()
+    # if the user isn't signed in give an empty checkout form so they can fill in their info
     else:
         order_form = OrderForm()
     context = {
@@ -47,25 +54,28 @@ def checkout(request, pk=None):
 
 
 def charge(request, pk=None):
+    """
+    this view creates a charge to be processed by stripe
+    by obtaining the amount from the form filled in the view checkout
+    """
     project = get_object_or_404(Project, pk=pk) if pk else None
 
     if request.method == 'POST':
         order_form = OrderForm(request.POST)
-        # PAYMENT
+        # CREATE PAYMENT INTENT and STRIPE DASHBOARD VARIABLES 
         total = str(float(request.POST['amount_pledged']))
         stripe_total = float(request.POST['amount_pledged'])*100
-        # print('Data:', request.POST)
-        print("stripe_total", stripe_total)
         customer = stripe.Customer.create(
             source=request.POST['stripeToken']
         )
+        # PAYMENT INTENT 
         intent = stripe.PaymentIntent.create(
             amount=int(stripe_total),
             currency="usd",
             payment_method_types=["card"],
             customer=customer,
         )
-
+        # PAYMENT CONFIRMATION
         confirm = stripe.PaymentIntent.confirm(
             intent.id,
             payment_method="pm_card_visa",
@@ -83,15 +93,22 @@ def charge(request, pk=None):
     else:
         order_form = OrderForm()
 
-
     return redirect(reverse('success', args=(total, pk, order.order_number)))
 
 
 def success(request, total, pk, order_number):
+    """
+    this view takes the values from the view charge AFTER the charge is deemed successfull
+    Only when it is successfull will this view start to save the user info entered by the user
+    and a confirmation email will be send with some details from the order.
+    Only after a successfull payment will the amount raised be increased for that specific project
+    """
+    # getting the amount, user info and order details
     amount = float(total)
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
+    # adding the order or (changed) user info to the profile page
     if request.user.is_authenticated:
         profile = UserProfile.objects.get(user=request.user)
         # Attach the user's profile to the order
@@ -111,11 +128,12 @@ def success(request, total, pk, order_number):
             user_profile_form = UserProfileForm(profile_data, instance=profile)
             if user_profile_form.is_valid():
                 user_profile_form.save()
-
+    # get the project for which a charge has been made and add the amount to the existing number raised for that project
     project = get_object_or_404(Project, pk=pk)
     project.raised += amount
     project.save(update_fields=["raised"])
 
+    # send a confirmation email with the order details to the user that made the order
     new_line ='\n'
     send_mail(
         f'Growing Funds: confirmation order {order_number}',
