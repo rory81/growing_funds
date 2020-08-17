734d22da2f976333efec67f9a3d9b390ae732ef8
Nathalie <nbleijie@gmail.com>
2020-08-17 22:16:33 +0000

    changed error settings.py

--- growing_funds/settings.py
+++ growing_funds/settings.py
@@ -15,14 +15,13 @@ import dj_database_url
 
 if os.path.exists('env.py'):
     import env
-    # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = True
-else:
-    DEBUG = False
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
