6f013d41f90e040ca2d67bb1c9a6d29a636964de
Nathalie <nbleijie@gmail.com>
2020-08-17 22:25:46 +0000

    changed Debug back to True due to Server 500 error

--- growing_funds/settings.py
+++ growing_funds/settings.py
@@ -15,14 +15,13 @@ import dj_database_url
 
 if os.path.exists('env.py'):
     import env
-    DEBUG = True
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
