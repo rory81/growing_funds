bb5a077ff614ce3dfd008c6972a9d21909c11a21
Nathalie <nbleijie@gmail.com>
2020-08-22 17:43:50 +0000

    changed font size mobile description and added tests_forms.py

---
+++ projects/tests_forms.py
@@ -0,0 +1,73 @@
+from django.test import TestCase
+from .forms import StartProjectForm
+
+
+class TestProjectForm(TestCase):
+    def test_project_title_is_required(self):
+        form = StartProjectForm({'title': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(form.errors['title'][0], 'This field is required.')
+
+    def test_project_category_is_required(self):
+        form = StartProjectForm({'category': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('category', form.errors.keys())
+        self.assertEqual(form.errors['category'][0], 'This field is required.')
+
+    def test_project_description_is_required(self):
+        form = StartProjectForm({'description': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('description', form.errors.keys())
+        self.assertEqual(form.errors['description'][0], 'This field is required.')
+
+    def test_project_backers_story1_is_required(self):
+        form = StartProjectForm({'backers_story_option1': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('backers_story_option1', form.errors.keys())
+        self.assertEqual(form.errors['backers_story_option1'][0], 'This field is required.')
+
+    def test_project_backers_story2_is_required(self):
+        form = StartProjectForm({'backers_story_option2': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('backers_story_option2', form.errors.keys())
+        self.assertEqual(form.errors['backers_story_option2'][0], 'This field is required.')
+
+    def test_project_backers_story3_is_required(self):
+        form = StartProjectForm({'backers_story_option3': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('backers_story_option3', form.errors.keys())
+        self.assertEqual(form.errors['backers_story_option3'][0], 'This field is required.')
+
+    def test_project_goal_is_required(self):
+        form = StartProjectForm({'goal': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('goal', form.errors.keys())
+        self.assertEqual(form.errors['goal'][0], 'This field is required.')
+
+    def test_project_end_date_is_required(self):
+        form = StartProjectForm({'end_date': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('end_date', form.errors.keys())
+        self.assertEqual(form.errors['end_date'][0], 'This field is required.')
+
+    def test_project_created_date_is_required(self):
+        form = StartProjectForm({'created_date': ''})
+        self.assertFalse(form.is_valid())
+
+    def test_project_num_raised_is_required(self):
+        form = StartProjectForm({'num_raised': ''})
+        self.assertFalse(form.is_valid())
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = StartProjectForm()
+        self.assertEqual(form.Meta.fields, [
+            'title',
+            'category',
+            'description',
+            'backers_story_option1',
+            'backers_story_option2',
+            'backers_story_option3',
+            'goal',
+            'end_date'
+        ])
